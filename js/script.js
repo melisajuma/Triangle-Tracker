bc874bd6fa1663fb40ea450c00ebdc0765ab52fc
melisajuma <Melisaakinyi95@gmail.com>
2019-06-03 10:42:52 +0300

    putting a condition that identifies a scalene triangle

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ if (a<=0 || b<=0 || c<=0){
     alert( "This is an equilateral triangle!");
 }else if (a+b<=c || b+c<=a || a+c<=b) {
     alert("Triangle CANNOT be formed!");
-}else if (a==b || b==c || c==a) {>
+}else if (a==b || b==c || c==a) {
     alert ( "This is an isosceles triangle!");
-} else if (!(a+b<=c || b+c<=a || a+c<=b ) && !(a===b && b===c && c===a)) {
+} else if (!(a+b<=c || b+c<=a || a+c<=b ) && !(a===b && b===c && c===a)) {>>
     alert("This is a scalene triangle!");
 } 
 }
