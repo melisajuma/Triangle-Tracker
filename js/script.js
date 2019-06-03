de5eed3a2bbd845871aab010a00a0fa8d5cd6612
melisajuma <Melisaakinyi95@gmail.com>
2019-06-03 10:38:45 +0300

    putting a condition that identifies an equilateral triangle

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ function triangle() {
 var a=parseInt(document.getElementById("a").value);
 var b=parseInt(document.getElementById("b").value);
 var c=parseInt(document.getElementById("c").value);
-if (a<=0 || b<=0 || c<=0){>
+if (a<=0 || b<=0 || c<=0){
     alert("Kindly check your values!");
-}else if (a===b && b===c && c===a) {
+}else if (a===b && b===c && c===a) {>>
     alert( "This is an equilateral triangle!");
 }else if (a+b<=c || b+c<=a || a+c<=b) {
     alert("Triangle CANNOT be formed!");
