7a671fd3152c71a2b824b7d02bdf5891cec45135
melisajuma <Melisaakinyi95@gmail.com>
2019-06-03 10:42:01 +0300

    putting a condition that identifies an isosceles triangle

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ if (a<=0 || b<=0 || c<=0){
     alert("Kindly check your values!");
 }else if (a===b && b===c && c===a) {
     alert( "This is an equilateral triangle!");
-}else if (a+b<=c || b+c<=a || a+c<=b) {>>?
+}else if (a+b<=c || b+c<=a || a+c<=b) {
     alert("Triangle CANNOT be formed!");
-}else if (a==b || b==c || c==a) {
+}else if (a==b || b==c || c==a) {>
     alert ( "This is an isosceles triangle!");
 } else if (!(a+b<=c || b+c<=a || a+c<=b ) && !(a===b && b===c && c===a)) {
     alert("This is a scalene triangle!");
